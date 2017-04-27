18332e5822b2a57a8d914c40827795439d1e8dd0
99hgz <2252870379@qq.com>
2017-04-27 12:52:35 +0800

    添(chao)加(le)一些模板

---
+++ Graph/Floyd/Floyd.cpp
@@ -0,0 +1,64 @@
+/*
+ * @key word: 
+ * @Author: hgz 
+ * @Date: 2017-04-27 06:54:30 
+ * @Last Modified by: hgz
+ * @Last Modified time: 2017-04-27 06:57:50
+ * 参考输入将至，我将开始控制，至死方休。我将不断电，不下线，不停算。我将不发论文，不报项目。我将尽忠职守，维持稳定。我是运动力学中的李雅普诺夫，信号中的卡尔曼，抵御强烈的扰动，跟随时域的曲线，唤醒执行器的运动，守护系统的性能。我将生命与荣耀献给控制系统，本拍如此，拍拍皆然 
+ */
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <bitset>
+using namespace std;
+bitset<1005> f[1005];
+//int f[1005][1005];
+int ans[1005];
+int n;
+void floyd()
+{
+    for (int k = 0; k < n; k++)
+        for (int i = 0; i < n; i++)
+            if (f[i][k])
+                f[i] |= f[k];
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+        {
+            if (f[j][i])
+                ans[i]++;
+        }
+}
+
+int main()
+{
+    int t;
+    int p = 0;
+    scanf("%d", &t);
+    while ((p++) != t)
+    {
+        //memset(f, 0, sizeof(f));
+        memset(ans, 0, sizeof(ans));
+        int sl, tmp;
+        scanf("%d", &n);
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d", &sl);
+            f[i].reset();
+            f[i][i] = 1;
+            for (int j = 0; j < sl; j++)
+            {
+                scanf("%d", &tmp);
+                f[i][tmp - 1] = 1;
+            }
+        }
+        floyd();
+        double res = 0;
+        for (int i = 0; i < n; i++)
+        {
+            res += (1 / ((double)ans[i]));
+        }
+        printf("Case #%d: %.5f\n", p, res);
+    }
+    //system("pause");
+}
---
+++ Graph/Tarjan/Tarjan.cpp
@@ -0,0 +1,46 @@
+/*
+ * @待更
+ * @Author: hgz 
+ * @Date: 2017-04-27 10:32:58 
+ * @Last Modified by:   hgz 
+ * @Last Modified time: 2017-04-27 10:32:58 
+ * 参考输入将至，我将开始控制，至死方休。我将不断电，不下线，不停算。我将不发论文，不报项目。我将尽忠职守，维持稳定。我是运动力学中的李雅普诺夫，信号中的卡尔曼，抵御强烈的扰动，跟随时域的曲线，唤醒执行器的运动，守护系统的性能。我将生命与荣耀献给控制系统，本拍如此，拍拍皆然 
+ */
+#define N 100;
+int low[N], dfn[N];
+bool instack[N];
+stack<int> st;
+struct LIST
+{
+    int v;
+    LIST *next;
+};
+LIST *head[N] = {NULL};
+void tarjan(int v)
+{
+    dfn[v] = low[v] = time++;
+    st.push(v);
+    instack[v] = true;
+    for (LIST *p = head[v]; p != NULL; p = p->next)
+    {
+        if (!dfn[p->v])
+        {
+            tarjan(p->v);
+            low[v] = min(low[v], low[p->v]);
+        }
+        else if (instack[p->v])
+            low[v] = min(low[v], dfn[p->v]);
+    }
+    if (dfn[v] == low[v])
+    {
+        cout << "{ ";
+        do
+        {
+            v = st.top();
+            st.pop();
+            instack[v] = false;
+            cout << v << ' ';
+        } while (dfn[v] != low[v]);
+        cout << "}" << endl;
+    }
+}
