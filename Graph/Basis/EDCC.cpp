a1e09f0e64d205e4e23546cb683e2faec03bcdfb
99hgz <2252870379@qq.com>
2017-07-10 22:12:23 +0800

    双联通分量

--- Graph/Basis/EDCC.cpp
+++ Graph/Basis/EDCC.cpp
@@ -1,28 +1,30 @@
 /*
- * @key word:割边，桥
- * @已测试:http://hihocoder.com/problemset/problem/1183
+ * @key word:边双联通分量
+ * @已测试:http://hihocoder.com/problemset/problem/1184
  * @Author: hgz
  * @Date: 2017-07-10 21:14:01
  * @Last Modified by: hgz
- * @Last Modified time: 2017-07-10 21:14:22
+ * @Last Modified time: 2017-07-10 22:08:33
  */
 #include <cstdio>
 #include <cstring>
 #include <cstdlib>
 #include <vector>
+#include <stack>
 #include <algorithm>
+#include <climits>
 using namespace std;
 typedef long long ll;
-int low[100100], dfn[100100];
-int n, m, a, b, root, time1;
+int low[100100], dfn[100100], ans[100100];
+int n, m, a, b, root, time1, dcc;
 bool visited[100100];
-vector<int> cut_vertex;
+
 vector<int> vec[100100];
 struct BRIDGE {
   int u, v;
 };
 vector<BRIDGE> bridge;
-
+stack<int> pointstack;
 void addedge(int a, int b) { vec[a].push_back(b); }
 bool cmp(BRIDGE a, BRIDGE b) {
   if (a.u == b.u)
@@ -34,6 +36,7 @@ void tarjan(int u, int fa) {
   low[u] = dfn[u] = ++time1;
   visited[u] = true;
   int son = 0;
+  pointstack.push(u);
   for (int i = 0; i < vec[u].size(); i++) {
     int v = vec[u][i];
     if (!visited[v]) {
@@ -46,11 +49,29 @@ void tarjan(int u, int fa) {
           swap(tu, tv);
         bridge.push_back((BRIDGE){tu, tv});
       }
-      if (((u == root) && (son > 1)) || ((u != root) && (dfn[u] <= low[v]))) {
-        cut_vertex.push_back(u);
-      }
+
     } else if (fa != v) {
       low[u] = min(low[u], dfn[v]);
+    }
+  }
+  if (low[u] == dfn[u]) {
+    stack<int> group;
+    dcc++;
+    int tmp, minst = INT_MAX;
+    while (!pointstack.empty()) {
+      // ans[pointstack.top()]=dcc;
+      tmp = pointstack.top();
+      group.push(tmp);
+      if (tmp < minst)
+        minst = tmp;
+      pointstack.pop();
+      if (tmp == u) {
+        break;
+      }
+    }
+    while (!group.empty()) {
+      ans[group.top()] = minst;
+      group.pop();
     }
   }
 }
@@ -68,21 +89,9 @@ int main() {
       tarjan(i, i);
     }
   }
-
-  sort(cut_vertex.begin(), cut_vertex.end());
-  cut_vertex.erase(unique(cut_vertex.begin(), cut_vertex.end()),
-                   cut_vertex.end());
-  sort(bridge.begin(), bridge.end(), cmp);
-  // printf("%d\n", cut_vertex.size());
-  if (cut_vertex.size() == 0) {
-    printf("Null\n");
-  }
-  for (int i = 0; i < cut_vertex.size(); i++) {
-    printf("%d ", cut_vertex[i]);
-  }
-  printf("\n");
-  for (int i = 0; i < bridge.size(); i++) {
-    printf("%d %d\n", bridge[i].u, bridge[i].v);
+  printf("%d\n", dcc);
+  for (int i = 1; i <= n; i++) {
+    printf("%d ", ans[i]);
   }
   system("pause");
   return 0;
