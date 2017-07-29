7ffa00c6f9ea4420b92a2ec9723cc16057a7d06c
99hgz <2252870379@qq.com>
2017-07-29 23:01:20 +0800

    bzoj水题

---
+++ Problems/bzoj/1002.py
@@ -0,0 +1,8 @@
+n=input()
+f=[]
+f.append(0)
+f.append(1)
+f.append(5)
+for i in range(3,n+1):
+    f[i]=f[i-1]*3-f[i-2]+2
+print (f[n])
---
+++ Problems/bzoj/7/F.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <algorithm>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    scanf("%d%d", &n, &m);
+    for (int i = 1; i <= n; i++)
+    {
+        scanf("%d%d", );
+    }
+    system("pause");
+    return 0;
+}
