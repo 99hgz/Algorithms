950781fe1272029e8e84bda7973aaf0a2d998f36
99hgz <2252870379@qq.com>
2017-10-16 21:55:53 +0800

    大整数类/stl测试

--- Basis/multiset.cpp
+++ Basis/multiset.cpp
@@ -6,23 +6,17 @@
 using namespace std;
 typedef long long ll;
 
-int main() {
+int main()
+{
   multiset<int> ms;
-
-  ms.insert(10);
-  ms.insert(13);
-  ms.insert(12);
-  ms.insert(11);
-  ms.insert(19);
-  ms.insert(13);
-  ms.insert(16);
-  ms.insert(17);
-  ms.insert(13);
-  for (int elem : ms) {
+  ms.insert(5);
+  ms.insert(9);
+  /*for (int elem : ms) {
     printf("%d ", elem);
-  }
+  }*/
   printf("\n");
-  printf("%d\n", *--ms.lower_bound(13));
+  printf("%d\n", *--ms.end());
+  printf("%d\n", *ms.lower_bound(6));
   system("pause");
   return 0;
 }
