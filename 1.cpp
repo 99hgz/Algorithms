1e0fbcc478dbda4c801d20495dc29db3519cd206
99hgz <2252870379@qq.com>
2017-12-13 21:45:31 +0800

    bzoj水题

--- 1.cpp
+++ 1.cpp
@@ -1,44 +1,163 @@
-#include <iostream>
-#include <cstdio>
+//  Created by Sengxian on 3/30/16.
+//  Copyright (c) 2016年 Sengxian. All rights reserved.
+//  BZOJ 3110 树套树
 #include <algorithm>
-#include <cmath>
+#include <iostream>
+#include <cctype>
+#include <cassert>
 #include <cstring>
+#include <cstdio>
+#include <vector>
 using namespace std;
-int n;
-struct node
+
+inline int ReadInt()
 {
-    long long x, y;
-    double z;
-} a[3005], b[3005];
-bool cmp(node a, node b)
-{
-    return a.z > b.z;
+    int ch = getchar(), n = 0;
+    bool flag = false;
+    while (!isdigit(ch))
+        flag |= ch == '-', ch = getchar();
+    while (isdigit(ch))
+        n = (n << 3) + (n << 1) + ch - '0', ch = getchar();
+    return flag ? -n : n;
 }
-bool cmp1(node a, node b) { return (a.y != b.y) ? a.y > b.y : a.x < b.x; }
+
+typedef long long ll;
+
+int n, m, vn, a, b, v;
+#define mid ((l + r) / 2)
+struct SegNode *null;
+struct SegNode
+{
+    SegNode *ls, *rs;
+    ll sum, addv;
+    SegNode() : ls(null), rs(null), sum(0), addv(0) {}
+    inline void plus(int len, int x) { sum += len * x, addv += x; }
+    inline void pushdown(int l, int r)
+    {
+        if (addv)
+            ls->plus(mid - l, addv), rs->plus(r - mid, addv), addv = 0;
+    }
+    inline void pushup() { sum = ls->sum + rs->sum; }
+    inline void maintain(int l, int r)
+    {
+        if (r - l == 1)
+            sum = addv;
+        else
+            sum = ls->sum + rs->sum + addv * (r - l);
+    }
+};
+
+void modify1D(SegNode *&o, int l, int r)
+{
+    if (l >= b || r <= a)
+        return;
+    if (o == null)
+        o = new SegNode();
+    if (l >= a && r <= b)
+        o->addv++;
+    else
+        modify1D(o->ls, l, mid), modify1D(o->rs, mid, r);
+    o->maintain(l, r);
+}
+
+ll query1D(SegNode *&o, int l, int r, ll add = 0)
+{
+    if (l >= b || r <= a)
+        return 0;
+    if (l >= a && r <= b)
+        return o->sum + add * (r - l);
+    return query1D(o->ls, l, mid, add + o->addv) + query1D(o->rs, mid, r, add + o->addv);
+}
+
+void build1D(SegNode *&o, int l, int r)
+{
+    o = new SegNode();
+    if (r - l > 1)
+        build1D(o->ls, l, mid), build1D(o->rs, mid, r);
+}
+
+struct SegNode2D *null2D;
+struct SegNode2D
+{
+    SegNode2D *ls, *rs;
+    SegNode *val;
+    SegNode2D() : ls(null2D), rs(null2D), val(null) {}
+} * root;
+
+void modify2D(SegNode2D *&o, int l, int r)
+{
+    if (o == null2D)
+        o = new SegNode2D();
+    if (r - l > 1)
+    {
+        if (v < mid)
+            modify2D(o->ls, l, mid);
+        else
+            modify2D(o->rs, mid, r);
+    }
+    modify1D(o->val, 0, n);
+}
+
+ll Query2D(SegNode2D *o, int l, int r, int k)
+{
+    if (r - l == 1)
+        return l;
+    ll s = query1D(o->rs->val, 0, n);
+    if (k <= s)
+        return Query2D(o->rs, mid, r, k);
+    else
+        return Query2D(o->ls, l, mid, k - s);
+}
+
+void init_null()
+{
+    null = new SegNode(), null->ls = null, null->rs = null, null->sum = null->addv = 0;
+    null2D = new SegNode2D(), null2D->ls = null2D, null2D->rs = null2D, null2D->val = null;
+    root = null2D;
+}
+
+const int maxm = 50000 + 3;
+vector<int> cs;
+int opers[maxm], as[maxm], bs[maxm], vs[maxm];
+
+int compress()
+{
+    for (int i = 0; i < m; ++i)
+    {
+        opers[i] = ReadInt(), as[i] = ReadInt(), bs[i] = ReadInt(), vs[i] = ReadInt();
+        if (opers[i] == 1)
+            cs.push_back(vs[i]);
+    }
+    sort(cs.begin(), cs.end());
+    cs.erase(unique(cs.begin(), cs.end()), cs.end());
+    for (int i = 0; i < m; ++i)
+        if (opers[i] == 1)
+            vs[i] = lower_bound(cs.begin(), cs.end(), vs[i]) - cs.begin();
+    return cs.size();
+}
+
 int main()
 {
-    scanf("%d", &n);
-    for (int i = 1; i <= n; i++)
-        scanf("%lld%lld", &a[i].x, &a[i].y);
-    sort(a + 1, a + n + 1, cmp1);
-    long long ans = 0;
-    for (int i = 1; i <= n; i++)
+    freopen("d:/tmp/ex_data.in", "r", stdin);
+    freopen("d:/tmp/ex_data.out", "w", stdout);
+
+    init_null();
+    n = ReadInt(), m = ReadInt();
+    vn = compress();
+    for (int i = 0; i < m; ++i)
     {
-        for (int j = 1; j <= i - 1; j++)
-            b[j].x = a[j].x, b[j].y = a[j].y;
-        for (int j = 1; j <= i - 1; j++)
-            b[j].x -= a[i].x, b[j].y -= a[i].y;
-        for (int j = 1; j <= i - 1; j++)
-            b[j].z = atan2(b[j].y, b[j].x);
-        sort(b + 1, b + i, cmp);
-        long long x = 0, y = 0;
-        for (int j = 1; j <= i - 1; j++)
+        int oper = opers[i];
+        a = as[i] - 1, b = bs[i], v = vs[i];
+        if (oper == 1)
         {
-            ans = ans + y * b[j].x - x * b[j].y;
-            x += b[j].x;
-            y += b[j].y;
+            modify2D(root, 0, vn);
         }
+        else if (oper == 2)
+        {
+            printf("%d\n", cs[Query2D(root, 0, vn, v)]);
+        }
+        else
+            assert(false);
     }
-    if (ans%2==1) printf("%lld.5\n",ans);else printf("%lld.0\n", ans);
     return 0;
 }
---
+++ Problems/bzoj/3110.cpp
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include <cstring>
+#include <cstdlib>
+#include <algorithm>
+using namespace std;
+typedef unsigned int ll;
+
+struct TREE
+{
+    int lazy, lson, rson;
+    ll sum;
+} tree[30000000];
+
+int tot, n, m, root[200010];
+
+void pushdown(int rt, int l, int r)
+{
+    if (tree[rt].lazy == 0)
+        return;
+    if (!tree[rt].lson)
+        tree[rt].lson = ++tot;
+    if (!tree[rt].rson)
+        tree[rt].rson = ++tot;
+    if (tree[rt].lazy != 0)
+    {
+        int mid = (l + r) >> 1;
+        tree[tree[rt].lson].lazy += tree[rt].lazy;
+        tree[tree[rt].rson].lazy += tree[rt].lazy;
+        tree[tree[rt].lson].sum += tree[rt].lazy * (mid - l + 1);
+        tree[tree[rt].rson].sum += tree[rt].lazy * (r - mid);
+        tree[rt].lazy = 0;
+    }
+}
+
+ll query_in(int &rt, int l, int r, int L, int R)
+{
+    //printf("query_in:rt=%d l=%d r=%d L=%d R=%d\n", rt, l, r, L, R);
+    if (l > R || r < L)
+        return 0;
+    if (rt == 0)
+        rt = ++tot;
+    if (L <= l && r <= R)
+        return tree[rt].sum;
+    pushdown(rt, l, r);
+    int mid = (l + r) >> 1;
+    return query_in(tree[rt].lson, l, mid, L, R) + query_in(tree[rt].rson, mid + 1, r, L, R);
+}
+
+void update_in(int &rt, int l, int r, int L, int R)
+{
+    //printf("update_in:rt=%d l=%d r=%d L=%d R=%d\n", rt, l, r, L, R);
+    if (l > R || r < L)
+        return;
+    if (rt == 0)
+        rt = ++tot;
+    if (L <= l && r <= R)
+    {
+        tree[rt].lazy += 1;
+        tree[rt].sum += (r - l + 1);
+        return;
+    }
+
+    pushdown(rt, l, r);
+    int mid = (l + r) >> 1;
+    tree[rt].sum += min(r, R) - max(l, L) + 1;
+    update_in(tree[rt].lson, l, mid, L, R);
+    update_in(tree[rt].rson, mid + 1, r, L, R);
+}
+
+void update_out(int rt, int l, int r, int x, int L, int R)
+{
+    //printf("update_out:rt=%d l=%d r=%d x=%d L=%d R=%d\n", rt, l, r, x, L, R);
+    if (!root[rt])
+        root[rt] = ++tot;
+    update_in(root[rt], 1, n, L, R);
+    if (l == r)
+        return;
+    int mid = (l + r) >> 1;
+    if (x <= mid)
+        update_out(rt * 2, l, mid, x, L, R);
+    else
+        update_out(rt * 2 + 1, mid + 1, r, x, L, R);
+}
+
+int ans, cases[100010], a[50010], b[50010], c[50010], Hash[50010], insertnum, tn;
+
+void work(int a, int b, ll c)
+{
+    int l = 1, r = tn, rt = 1, omid = 0;
+    while (l <= r)
+    {
+        int mid = (l + r) >> 1;
+        if (mid == omid)
+            return;
+        ll Rank = query_in(root[rt * 2 + 1], 1, n, a, b);
+        if (c > Rank)
+            ans = mid, rt = rt * 2, r = mid, c -= Rank;
+        else
+            rt = rt * 2 + 1, l = mid + 1;
+        omid = mid;
+    }
+}
+
+int main()
+{
+
+    scanf("%d%d", &n, &m);
+    for (int i = 1; i <= m; i++)
+    {
+        scanf("%d%d%d%d", &cases[i], &a[i], &b[i], &c[i]);
+        if (cases[i] == 1)
+            Hash[++insertnum] = c[i];
+    }
+    sort(Hash + 1, Hash + 1 + insertnum);
+    tn = unique(Hash + 1, Hash + 1 + insertnum) - Hash - 1;
+    for (int i = 1; i <= m; i++)
+        if (cases[i] == 1)
+            c[i] = lower_bound(Hash + 1, Hash + 1 + tn, c[i]) - Hash;
+    for (int i = 1; i <= m; i++)
+    {
+        if (cases[i] == 1)
+            update_out(1, 1, tn, c[i], a[i], b[i]);
+        else
+        {
+            ans = 0;
+            work(a[i], b[i], c[i]);
+            printf("%d\n", Hash[ans]);
+        }
+    }
+    system("pause");
+    return 0;
+}
